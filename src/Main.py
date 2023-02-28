56d07d7bd56975e76561e8a7f260a3584bc3cc4f
JZP930506 <jzp930506@163.com>
2023-03-01 00:01:17 +0800

    ft:[20230301] add the main

---
+++ src/Main.py
@@ -0,0 +1,11 @@
+from opencv_project.src.Bisection import Bisection
+
+
+class Main:
+    def __int__(self):
+        bisect = Bisection()
+        bisect.PickBisectionMatrix()
+
+
+p = Main()
+p.__int__()
