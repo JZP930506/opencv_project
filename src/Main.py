1f3d3a477639f1c70cb9a5aa1432a6c3ab4d4151
JZP930506 <jzp930506@163.com>
2023-03-02 23:24:18 +0800

    ft:[cnbc-20230302] add the spec

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,13 @@
 from opencv_project.src.Bisection import Bisection
+from opencv_project.src.spce import Spliter
 
 
 class Main:
     def __int__(self):
         bisect = Bisection()
         bisect.PickBisectionMatrix()
+        spliter = Spliter()
+        spliter.ShowSpliterPicture()
 
 
 p = Main()
--- src/spce.py
+++ src/spce.py
@@ -2,16 +2,19 @@ import cv2
 import matplotlib.pyplot as pylab
 
 class Spliter:
-    def showSpliterPicture(path):
-        lena = cv2.imread('../images/black.png')
+    @staticmethod
+    def ShowSpliterPicture():
+        lena = cv2.imread('../images/cat.jpg')
+        pylab.subplot(1, 4, 1)
+        pylab.imshow(lena)
         lena_double = lena+lena
         b, g, r = cv2.split(lena)  # 将图像分为b,g,r三部分
         bgr = cv2.merge([b, g, r])  # 按照bgr顺序合并图像
         rgb = cv2.merge([r, g, b])  # 按照rgb顺序合并图像
-        pylab.subplot(1, 2, 1)
+        pylab.subplot(1, 4, 2)
         pylab.imshow(bgr)
-        pylab.subplot(1, 2, 2)
+        pylab.subplot(1, 4, 3)
         pylab.imshow(rgb)
+        pylab.subplot(1, 4, 4)
         pylab.imshow(lena_double)
         pylab.show()
-        cv2.waitKey(0)
